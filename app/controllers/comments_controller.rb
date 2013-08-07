1516ca52d7efb54a6f1d9e9440b13a731b6dd8e3
Rosieva Ashong-Katai <rosenaadedei@aol.com>
2013-08-07 16:14:22 -0400

    This is able to comment, but lyrics do not show up on the page.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,12 @@
 class CommentsController < ApplicationController
+def new
+@comment =Comment.new
+end
+
+def index
+    @comments = Comment.all
+  end
+
 	def create
 	  @comment = Comment.new(comment_params)
 	  @comment.article_id = params[:article_id]
@@ -7,6 +15,13 @@ class CommentsController < ApplicationController
 
 	  redirect_to article_path(@comment.article)
 	end
+
+def update
+    @comment = Comment.find(params[:id])
+    @comment.update(comment_params)
+    flash.notice = "Comment updated!"
+    redirect_to comment_path(@comment)
+  end
 
 	def comment_params
 	  params.require(:comment).permit(:author_name, :body)
